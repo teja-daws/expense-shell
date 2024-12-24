ba16398333c0c8dfa1e928595a45ef5564a36f74
teja-daws <tejadevops94@gmail.com>
2024-12-24 10:36:07 +0530

    modifying redirectors script

---
+++ mysql.sh
@@ -0,0 +1,50 @@
+#!bin/bash
+
+LOGS_FOLDER="/var/log/expense"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME/$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
+
+USERID=$(id -u)
+#echo "User ID is: $USERID"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is...$R FAILED $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is...$G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
+USAGE(){
+    echo -e "$R USAGE:: $N sudo sh 16-redirectors.sh package1 package2..." | tee -a $LOG_FILE
+    exit 1
+}
+
+echo "Script started excuting at $(date)" | tee -a $LOG_FILE
+
+CHECK_ROOT
+
+dnf install mysql-server -y
+VALIDATE $? "Installing MYSQL Server"
+
+systemctl enable mysqld
+VALIDATE $? "Enabled MYSQL"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1
+VALIDATE $? "Setting up for root password"

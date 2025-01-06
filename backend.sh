81a7609ae1b201b2813b7feba7db86ed04b394b2
teja-daws <tejadevops94@gmail.com>
2025-01-06 08:24:50 +0530

    modifying shell script

--- backend.sh
+++ backend.sh
@@ -60,5 +60,6 @@ curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expen
 VALIDATE $? "Downloading the backend application code"
 
 cd /app
-unzip /tmp/backend.zip
+rm -rf /app/* #It will removes the entire code
+unzip /tmp/backend.zip &>>LOG_FILE
 VALIDATE $? "Extracting backend application code"

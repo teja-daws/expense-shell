aaafd50f1d0cb7379bade65efd3be5831707b76f
teja-daws <tejadevops94@gmail.com>
2025-01-06 12:31:11 +0530

    shell

--- frontend.sh
+++ frontend.sh
@@ -53,5 +53,7 @@ cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>LOG_FILE
 VALIDATE $? "Extract frontend code"
 
+cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf
+
 systemctl restart nginx
 VALIDATE $? "restarted Nginx"
